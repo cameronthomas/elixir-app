4d7384522ac237f01cd49ded061166396500ac00
Cameron <cct2491@gmail.com>
2019-05-27 19:17:51 -0600

    Added another page.

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -17,7 +17,8 @@ defmodule AppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
-    get "/hello", HelloController, :index
+    get "/app", AppController, :index
+    get "/app:messenger", AppController, :index
   end
 
   # Other scopes may use custom stacks.
